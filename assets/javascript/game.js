93b4448a0fcca5c796e819b103af9f48d6e8d5d0
Devanshi Mathur <devanshimathur@Yuvrajs-MacBook-Pro.local>
2019-02-19 19:00:16 -0800

    done with most logic but glitches with repeat clicks

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,49 +5,66 @@ var pic1;
 var pic2;
 var pic3;
 var pic4;
-
-
+var wins=0;
+var loss=0;
 ///////////////////////////////////////ARRAYS///////////////////////////////////////
 
 
 ///////////////////////////////////////FUNCTIONS////////////////////////////////////
 //---------------------------------------------------------  creates a random number to get to  ##2
 function getMainNum (){
+    console.log("YOU ARE IN GET MAIN NUM");
     mainNum=parseInt(Math.random()*100);
+    //console.log("in getMainNum", mainNum);
+    if(mainNum>120){
+        getMainNum();
+    }
+    if(mainNum<19){
+        getMainNum();
+    }
+    //console.log("after the checks in getMainNum", mainNum);
     return mainNum;
     // console.log("mainNum", mainNum);
     // console.log("playerNum", playerNum);
 }
 //---------------------------------------------------------  gives buttons thier values   ##3
 function assignNumToPic (pic, mainNum){
-    pic= parseInt(mainNum*Math.random()*.80);
-    console.log("pic", pic, "mainNum", mainNum);
+    console.log("YOU ARE ASSIGN NUM TO PIC");
+    var i=0;
+    i=parseInt(1+mainNum*Math.random()*.80);
+        //console.log("assignNumToPic i before =", i);
+    if (i>12){
+        i= assignNumToPic(i,mainNum);
+        //console.log("in the if loop", i);
+    };
+    pic=i;
     return pic;
 }
 //---------------------------------------------------------  takes the click and adds it to playernum  ##4
 function addNum(){
-    console.log("Yup! You did it babe!");
-    console.log($(this).attr("id"));
+    console.log("YOU ARE IN ADD NUM");
+    //console.log("Yup! You did it babe!");
+    //console.log($(this).attr("id"));
     switch ($(this).attr("id")){
         case 'img1':
-            console.log("You clicked image 1 babe!");
+            // console.log("You clicked image 1 babe!");
             playerNum= playerNum+pic1;
-            console.log("playerNum in switch", playerNum)
+            // console.log("playerNum in switch", playerNum)
             break;
         case 'img2':
-            console.log("You did ittttttt pic2!");
+            // console.log("You did ittttttt pic2!");
             playerNum= playerNum+pic2;
-            console.log("playerNum in switch", playerNum)
+            // console.log("playerNum in switch", playerNum)
             break;
         case 'img3':
-            console.log("guessed what you diddddd pic3");
+            // console.log("guessed what you diddddd pic3");
             playerNum= playerNum+pic3;
-            console.log("playerNum in switch", playerNum)
+            // console.log("playerNum in switch", playerNum)
             break;
         case 'img4':
-            console.log("yup yup yup img 44444");
+            // console.log("yup yup yup img 44444");
             playerNum= playerNum+pic4;
-            console.log("playerNum in switch", playerNum)
+            // console.log("playerNum in switch", playerNum)
             break;
     }
     $("#playerNum").empty();
@@ -55,26 +72,76 @@ function addNum(){
     checkValue(playerNum);
 }
 //---------------------------------------------------------  checks the playerNum against mainNum ##5
-function checkValue(){
-    console.log("got into checkValue()")
+function checkValue(playerNum){
+    console.log("YOU GOT INTO CHECK VALUE)");
+    if (playerNum==mainNum){
+        alert("You won muthafucker!!!");
+        wins++;
+        console.log("YOU WON LOOP");
+        reset();
+    }
+    else if (playerNum>mainNum){
+        alert("You lost muthafucker :(  ");
+        loss++;
+        console.log("YOU LOST LOOP");
+        reset();
+    }
+    console.log("done with checkValue");
 }
-//---------------------------------------------------------  main order of events, starting point  ## 1
-function startUp(){
-    playerNum=0;
+//---------------------------------------------------------  resets the game for another round ## 6
+function reset(){
+    console.log("YOU GOT INTO RESET");
+    $("#mainNum").empty();
+    $("#playerNum").empty();
+    $("#wins").empty();
+    $("#loss").empty();
+
+    playerNum=0; 
     mainNum= getMainNum();
-        //console.log("mainNum = ", mainNum);
     pic1=assignNumToPic(pic1, mainNum);
-        //console.log("pic1=", pic1);
     pic2=assignNumToPic(pic2, mainNum);
-        //console.log("pic2=", pic2);
     pic3=assignNumToPic(pic3, mainNum);
-        //console.log("pic3=", pic3);
     pic4=assignNumToPic(pic4, mainNum);
-        //console.log("pic2=", pic4);
+        
+
     $("#mainNum").append(mainNum);
     $("#playerNum").append(playerNum);
+    $("#wins").append(wins);
+    $("#loss").append(loss);
+
     $("img").click(addNum);
-    console.log("playerNum in startUp", playerNum);
-    
+
+    // console.log("playerNum in reset", playerNum);
+
+    // console.log("wins=", wins, "loss", loss);
+    // console.log("mainNum = ", mainNum);
+    // console.log("pic1=", pic1);
+    // console.log("pic2=", pic2);
+    // console.log("pic3=", pic3);
+    // console.log("pic4=", pic4);
+}
+//---------------------------------------------------------  main order of events, starting point  ##1
+function startUp(){
+    console.log("YOU ARE IN STARTUP");
+    playerNum=0;
+    mainNum= getMainNum();
+    pic1=assignNumToPic(pic1, mainNum);
+    pic2=assignNumToPic(pic2, mainNum);
+    pic3=assignNumToPic(pic3, mainNum);
+    pic4=assignNumToPic(pic4, mainNum);
+        
+    $("#mainNum").append(mainNum);
+    $("#playerNum").append(playerNum);
+    $("#wins").append(wins);
+    $("#loss").append(loss);
+    $("img").click(addNum);
+
+    // console.log("wins=", wins, "loss", loss);
+    // console.log("mainNum = ", mainNum);
+    // console.log("playerNum in startUp", playerNum);
+    // console.log("pic1=", pic1);
+    // console.log("pic2=", pic2);
+    // console.log("pic3=", pic3);
+    // console.log("pic4=", pic4);
 }
 
